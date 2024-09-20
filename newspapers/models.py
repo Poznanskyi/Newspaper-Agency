35e3a89fa80041cae1debd4059880dbb885bb246
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-09-20 11:40:23 +0300

    add to newspapers/models: class Newspaper

--- newspapers/models.py
+++ newspapers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    publish_date = models.DateField(auto_now_add=True)
+    topic = models.ForeignKey(
+        Topic, on_delete=models.CASCADE, related_name='newspapers'
+    )
+    publishers = models.ManyToManyField(
+        Redactor, related_name='newspapers'
+    )
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
