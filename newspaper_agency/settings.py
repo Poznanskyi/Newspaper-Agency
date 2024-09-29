21a7f73415a0a86a10335705eaacd2e93b0c270e
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-09-29 23:12:25 +0300

    add .gitkeep

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get('DJANGO_SECRET-KEY', 'django-insecure-&yotx%#ria7dvb@@h_*57%*=q6annri+h@#=)^cpfjq^1iq)yg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'newspaper-agency-cict.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
